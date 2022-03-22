7782de0427c97fec705176f522e3ef2eee16f551
nwasolujohn <97901914+nwasolujohn@users.noreply.github.com>
2022-03-22 19:44:25 +0100

    Create CovidETL.sh

---
+++ ~/CovidETL.sh
@@ -0,0 +1,3 @@
+wget -O /var/lib/mysql-files/CovidData.csv https://data.edmonton.ca/api/views/ix8f-s9xp/rows.csv?accessType=DOWNLOAD
+mysql < C/Users/nwaso/Desktop/work/git/covid.sql
+rm /C/Users/nwaso/Desktop/work/git/COVID-19_in_Alberta__Current_cases_by_local_geographic_area.csv
